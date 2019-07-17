554125e34e374904530e12ae5f0aa04ffc7175f2
Travis Personal <travispersonal@l-goodwin-travis.ptp>
2019-07-16 18:02:54 -0700

    Add Splash screen, timer script

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+
+    public float seconds;
+
+    private float targetTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetTime = Time.timeSinceLevelLoad + seconds;
+        Debug.Log(targetTime + " Is Target Time");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.timeSinceLevelLoad >= targetTime) {
+            Debug.Log("Quitting!");
+        }
+    }
+}
---
+++ Assets/Splash/Splash.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class Splash : MonoBehaviour
+{
+    private VideoPlayer splash;
+
+    void Start()
+    {
+        splash = GetComponent<VideoPlayer>();
+        splash.loopPointReached += EndReached;    
+    }
+
+    void Update() {
+        // Any button press goes here to advance
+    }
+
+    void EndReached(VideoPlayer vplayer) 
+    {
+        SceneManager.LoadScene("Main");
+    }    
+}
