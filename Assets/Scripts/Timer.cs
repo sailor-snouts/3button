c5c1430daa52dfd07583b7a9d4a7928f905d62f0
Travis Personal <travispersonal@l-goodwin-travis.ptp>
2019-07-16 18:39:58 -0700

    Add timer canvas prefab, add prefab to main

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
 
     public float seconds = 300;
-    
+
+    [SerializeField]
+    private TextMeshProUGUI timerCountdownText;
+
     private bool hasEnded = false;
 
     // Start is called before the first frame update
@@ -18,6 +23,7 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        timerCountdownText.text = Math.Truncate(seconds - Time.timeSinceLevelLoad).ToString();
         if(Time.timeSinceLevelLoad >= seconds && !hasEnded) {
             Debug.Log("Quitting!");
             hasEnded = true;
