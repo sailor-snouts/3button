4db8dcf5ad532a812ce33273274f5e82d0e2b890
Robert Parker <rparker@kodama.design>
2019-07-16 18:20:35 -0700

    textmesh pro and scene fading

---
+++ Assets/Scripts/MetersController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetersController : MonoBehaviour
+{
+    private MeterController[] meters;
+
+    void Start()
+    {
+        this.meters = GetComponentsInChildren<MeterController>();
+    }
+    
+    void Update()
+    {
+        foreach(MeterController meter in this.meters)
+        {
+            if(!meter.IsStable())
+            {
+                FindObjectOfType<SceneChange>().FadeToLevel("GameOver");
+            }
+        }
+    }
+}
