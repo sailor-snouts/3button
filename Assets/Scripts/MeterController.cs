dc82edf1068cb53d5cf53ddfd4ad265abdd81a8f
Robert Parker <rparker@kodama.design>
2019-07-16 17:36:26 -0700

    starting meter

---
+++ Assets/Scripts/MeterController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeterController : MonoBehaviour
+{
+    private float current;
+    private float target;
+    private float button1;
+    private float button2;
+    private float button3;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Joystick1Button0) || Input.GetKeyDown(KeyCode.Q))
+        {
+            this.current += this.button1;
+        }
+        if (Input.GetKeyDown(KeyCode.Joystick1Button1) || Input.GetKeyDown(KeyCode.W))
+        {
+            this.current += this.button2;
+        }
+        if (Input.GetKeyDown(KeyCode.Joystick1Button2) || Input.GetKeyDown(KeyCode.E))
+        {
+            this.current += this.button3;
+        }
+    }
+
+
+}
