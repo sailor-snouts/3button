6f813a34ac14f1289051f71e677287434a892264
Robert Parker <rparker@kodama.design>
2019-07-20 20:38:47 -0700

    meter with animation, score changes

--- Assets/Scripts/MeterController.cs
+++ Assets/Scripts/MeterController.cs
@@ -2,16 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class MeterController : MonoBehaviour
 {
     [SerializeField]
     private float current;
-
-    [SerializeField]
-    private float target;
-
-    [SerializeField]
-    private float tolerance;
 
     [SerializeField]
     private float button1;
@@ -22,39 +17,48 @@ public class MeterController : MonoBehaviour
     [SerializeField]
     private float button3;
 
+    [SerializeField]
+    private float button4;
+
+    private Animator anim;
+
+    private void Start()
+    {
+        this.anim = GetComponent<Animator>();
+        this.anim.SetFloat("Value", this.current);
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Joystick1Button0) || Input.GetKeyDown(KeyCode.Q))
         {
             this.current += this.button1;
+            this.anim.SetFloat("Value", this.current);
         }
         if (Input.GetKeyDown(KeyCode.Joystick1Button1) || Input.GetKeyDown(KeyCode.W))
         {
             this.current += this.button2;
+            this.anim.SetFloat("Value", this.current);
         }
         if (Input.GetKeyDown(KeyCode.Joystick1Button2) || Input.GetKeyDown(KeyCode.E))
         {
             this.current += this.button3;
+            this.anim.SetFloat("Value", this.current);
+        }
+        if (Input.GetKeyDown(KeyCode.Joystick1Button3) || Input.GetKeyDown(KeyCode.R))
+        {
+            this.current += this.button4;
+            this.anim.SetFloat("Value", this.current);
         }
     }
 
     public bool IsStable()
     {
-        return Mathf.Abs(this.current - this.target) <= this.tolerance;
+        return Mathf.Abs(this.current) <= 10;
     }
 
     public float GetCurrent()
     {
         return this.current;
-    }
-
-    public float GetTarget()
-    {
-        return this.target;
-    }
-
-    public void SetTarget(float target)
-    {
-        this.target = target;
     }
 }
---
+++ Assets/Scripts/ScoreGui.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreGui : MonoBehaviour
+{
+    private Score score;
+
+    [SerializeField]
+    TextMeshProUGUI scoreText;
+
+    private void Start()
+    {
+        this.score = FindObjectOfType<Score>();
+    }
+
+    private void OnGUI()
+    {
+        this.scoreText.SetText("{0}", this.score.GetValue());
+    }
+}
